78e7de2da4c016f94ddcb2bd48f43857c12f7f91
Hafidz Athaya <mhafidza528@gmail.com>
2025-08-12 18:01:44 +0800

    Add: CORS

--- middleware/cors_middleware.go
+++ middleware/cors_middleware.go
@@ -10,9 +10,9 @@ func CORSMiddleware(next http.Handler) http.Handler {
 			"https://himati-e-election-polnes.vercel.app":                         true,
 			"https://himati-e-election-polnes-alvin2005-gits-projects.vercel.app": true,
 			"https://m-hafidz-athaya-e-election.vercel.app":                       true,
-			"http://127.0.0.1":      true,
-			"http://localhost:5500": true,
-			"http://127.0.0.1:5500": true,
+			"http://localhost:5500":                                               true,
+			"http://127.0.0.1:5500":                                               true,
+			"*":                                                                   true,
 		}
 
 		if allowedOrigins[origin] {
